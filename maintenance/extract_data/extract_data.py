55e4c63c71b5c3ef1190c3b59bf2988a53da4ae7
traveller195 <theo92@gmx.net>
2024-02-19 09:44:06 +0100

    change data extract to include also geometry data, but not finished yet. also care about correct SQL query without JOIN between tables

--- maintenance/extract_data/extract_data.py
+++ maintenance/extract_data/extract_data.py
@@ -9,6 +9,11 @@ import zipfile
 
 import psycopg2
 
+from osgeo import gdal
+from osgeo import ogr
+from osgeo import osr
+from osgeo import gdal_array
+from osgeo import gdalconst
 
 class ZipFileExistsError(Exception):
     pass
@@ -20,6 +25,9 @@ def get_connection():
         user=os.environ['PGUSER'],
         password=os.environ['PGPASSWORD']
     )
+
+def get_connection_string_for_gdal():
+    return 'PG:host={host} dbname={dbname} user={username} password={password} port=5432'.format(host=os.environ['PGHOST'], dbname=os.environ['PGDATABASE'], username=os.environ['PGUSER'], password=os.environ['PGPASSWORD'])
 
 
 def get_extract_zip_file_path(current_time):
@@ -46,12 +54,37 @@ def read_sql(rel_path_from_script):
     sql_path = script_directory / rel_path_from_script
     return sql_path.read_text()
 
+def make_data_extract_geopackage():
 
+    from osgeo import gdal
+    gdal.UseExceptions()
+    gdal.SetConfigOption('CPL_DEBUG', 'ON')
+
+
+    ds = gdal.OpenEx(get_connection_string_for_gdal(), gdal.OF_VECTOR)
+
+    gdal.VectorTranslate(
+        '/tmp/building_attributes_geometry.gpkg',
+        ds,
+        options=gdal.VectorTranslateOptions(
+            SQLStatement='SELECT blds.*, geoms.geometry_geom AS "geometry" FROM public.buildings blds, public.geometries geoms WHERE blds.re_toid=geoms.source_id',
+            layerName='buildings_with_geom',
+            format='GPKG'
+        )
+    )
+
+    ds = None
+
+    return
 
 
 def make_data_extract(current_time, connection, zip_file_path):
     if zip_file_path.exists():
         raise ZipFileExistsError('Archive file under specified name already exists')
+
+    # extract attribute data with geometry in Geopackage format using GDAL
+    make_data_extract_geopackage()
+
 
     # Execute data dump as Postgres COPY commands, write from server to /tmp
     with connection.cursor() as cur:
@@ -70,7 +103,8 @@ def make_data_extract(current_time, connection, zip_file_path):
     try:
         with zipfile.ZipFile(zip_file_path, mode='w') as newzip:
             newzip.write(source_dir_path / 'README.md', arcname='README.md')
-            newzip.write('/tmp/building_attributes.csv', arcname='building_attributes.csv')
+            newzip.write('/tmp/building_attributes_geometry.gpkg', arcname='building_attributes_geometry.gpkg')
+            #newzip.write('/tmp/building_attributes.csv', arcname='building_attributes.csv')
             #newzip.write('/tmp/building_uprns.csv', arcname='building_uprns.csv')
             newzip.write('/tmp/edit_history.csv', arcname='edit_history.csv')
 
